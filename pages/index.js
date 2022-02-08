a269918ed6bcc50ec50f95161b9b70d197d1ebdd
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-02-08 14:54:30 -0300

    Primeiro teste com sucesso

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
-import Link from 'next/link';
+import {  useState } from 'react';
 
 function Home() {
-    return (<div>
-        <h1>Home</h1>
+    return (
+        <div>
+            <h1>Bem-vindo!</h1>
+            <Contador/>        
+            <div>Clique para somar</div>        
+        </div>
+    )
+}
 
-        <Link href="/sobre">
-            <a>Acessar página Sobre</a>
-        </Link>
-    </div>)
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+        
+        return (
+            <div>
+                <div>{contador}</div>
+                <button onClick={adicionarContador}>Adicionar</button>
+                </div>
+            
+    
+        )
+    
 }
 
 export default Home
